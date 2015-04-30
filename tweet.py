c3261134eb27cca5eab7cef9219bb96da062229c
Andrew Clarkson <andrew@clarkson.mn>
2015-04-30 11:33:33 -0500

    dumb typo

--- tweet.py
+++ tweet.py
@@ -10,4 +10,4 @@ def index():
 if __name__ == '__main__':
     app.debug = True
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=port)
