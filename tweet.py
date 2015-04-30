821f61e7b91c847d45cbda302b11eaa353372214
Andrew Clarkson <andrew@clarkson.mn>
2015-04-30 15:36:40 -0500

    switch libraries

--- tweet.py
+++ tweet.py
@@ -57,7 +57,6 @@ def create():
 
 if __name__ == '__main__':
     app.debug = True
-    db.connect()
     db.create_tables([Tweet], safe=True)
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
